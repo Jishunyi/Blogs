546dfc8e250abd5255c92ab36756febcf08c4526
Jishunyi <jishunyi131@163.com>
2020-05-20 12:11:49 +0800

    Short to Char

--- ShortToChar/ShortToChar.c
+++ ShortToChar/ShortToChar.c
@@ -6,7 +6,8 @@ unsigned char *ShortToChar(const unsigned short *ShortData, unsigned int Lenth)
     int i, j;
     unsigned char *CharData;
     CharData = (unsigned char *)malloc(Lenth * sizeof(unsigned char));
-    for (i = 0, j = 0; i < Lenth; i + 2, j++)
+    printf("ShortData Size: %zd Byte.\n", Lenth);
+    for (i = 0, j = 0; i < Lenth; i += 2, j++) //i+2是错误的
     {
         CharData[i] = ShortData[j] & 0xff;            //低八位 00
         CharData[i + 1] = (ShortData[j] >> 8) & 0xff; //高八位 11
@@ -17,13 +18,12 @@ unsigned char *ShortToChar(const unsigned short *ShortData, unsigned int Lenth)
 int main(void)
 {
     int i;
-    unsigned short ShortData[4] = {0x0011, 0x2233, 0x4455, 0x6677};
+    unsigned short ShortData[4] = {0x0011223344556677};
     unsigned char *CharData;
-    printf("ShortData Size: %zd Byte.\n", sizeof(ShortData));
-    CharData = ShortToChar(ShortData, (unsigned int)sizeof(ShortData));
+    CharData = ShortToChar(ShortData, sizeof(ShortData));
     for (i = 0; i < (unsigned int)sizeof(ShortData); i++)
     {
-        printf("%#x ", CharData[i]);
+        printf("%x ", CharData[i]);
     }
 
     return 0;
